e5045d265aeaddc2cb3f525a2650fbfd46c1b52d
velicharlagokulkumar <velicharlagokulkumar@gmail.com>
2024-06-02 15:18:39 +0530

    updated

--- project_3/project_3.srcs/scripts/build.tcl
+++ project_3/project_3.srcs/scripts/build.tcl
@@ -1,8 +1,8 @@
 # read design sources (add one line for each file)
-read_verilog -v "../project_3.srcs/sources_1/bd/Base_Zynq_MPSoC/hdl/Base_Zynq_MPSoC_wrapper.v"
+read_verilog -v "../sources_1/bd/Base_Zynq_MPSoC/hdl/Base_Zynq_MPSoC_wrapper.v"
 
 # read constraints
-read_xdc "../project_3.srcs/constrs_1/new/top.xdc"
+read_xdc "../constrs_1/new/top.xdc"
 
 # synth
 synth_design -top "Base_Zynq_MPSoC_wrapper" -part "xczu28dr-ffvg1517-2-e"

b7e0d7c02b3d9d1d9edcc5b0a4ed81a521885ecc
velicharlagokulkumar <velicharlagokulkumar@gmail.com>
2026-01-30 15:33:18 +0530

    updated

--- D_ff/sim/d_ff_tb.sv
+++ D_ff/sim/d_ff_tb.sv
@@ -4,6 +4,7 @@ module d_ff_tb(
     
 logic clk;
 logic rst_n;
+logic set_n;
 logic [2:0]d_ff;
 logic [2:0] d_in;
 
@@ -12,6 +13,7 @@ d_ff inst_ff (.*);
 initial begin
     clk = 0;
     rst_n = 0;
+    set_n = 1;
     d_in = 'd0;
     # 20 rst_n = 1;
       d_in = 'd5;
@@ -26,6 +28,7 @@ end
 
 initial begin
  #30 rst_n = 0;
+ #2 set_n = 0;
  #8 rst_n = 1;
 end
 
--- D_ff/src/d_ff.sv
+++ D_ff/src/d_ff.sv
@@ -3,21 +3,35 @@
 module d_ff(
    input clk,
    input rst_n,
+   input set_n,
    input [2:0] d_in,
    output logic [2:0] d_ff
     );
     
 logic [2:0] d_ff_2 ;
     
-always_ff @ (posedge clk or negedge rst_n) begin
+always_ff @ (posedge clk or negedge rst_n or negedge set_n) begin
   if(~rst_n) begin
-    d_ff <= 0;
-    d_ff_2 <= 0;
+    d_ff <= 'd0;
   end 
+  else if(set_n) begin
+    d_ff <= 'b111;
+  end
   else begin
     d_ff <= d_in;
-    d_ff_2 <= d_ff;
  end
  end
+ 
+ // follower flop's
+ always_ff @ (posedge clk)
+    d_ff_2 <= d_ff; 
+    
+ // synopsys  translate_off
+ always @(rst_n or set_n)
+ if (rst_n && !set_n)
+  force d_ff = 1;
+ else 
+ release d_ff;
+ // synopsys translate_on
 
 endmodule
